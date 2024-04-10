c7cdc24c8236d9478012d1b477d70a2a84e73e37
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-04-10 19:54:06 +0200

    Feat: section logich to script

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
 //INPUT
-// definisco la variabile conteggio
-let countD = 0;
 // ottengo l'elemento HTML con id "secondCountDown"
 const counterSc = document.getElementById("secondCd");
-// Aggiorno il contenute dell elemento HTML con il contenuto di countD
+const happyYear = document.getElementById("happyNewYear");
 
+// definisco la variabile conteggio
+let countD = 0;
 
-
-
+// eve
 const btnCd = document.getElementById("btnCd").addEventListener("click", function(){
     countD = 10;
     
 const clock = setInterval(function(){
-   countD --;
-   if(countD === 0) {
+   // Aggiorno il contenute dell elemento HTML con il contenuto di countD 
+   counterSc.innerHTML = countD --;
+   // condizione se variabile conteggio è uguale a -1 applica clearInver 
+   // bloccherà setInterval
+   if(countD === -1) {
+    let countentMess = "Buon Anno!";
+    happyYear.innerHTML = countentMess;
     clearInterval(clock)
-    console.log("buon Anno!")
+    
+    
    }
    console.log(countD)
 },1000);
     
 });
+
+    
+
+
+
 
 
 
